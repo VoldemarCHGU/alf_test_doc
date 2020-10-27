36b73689d97613a44149868b28d2e71e94001506
admin <vovnik98@mail.ru>
2020-10-27 03:04:00 +0300

    Подчсчёт тестов для каждого тарифа в json

--- pages/knowledge/proverka_file_json.py
+++ pages/knowledge/proverka_file_json.py
@@ -1,6 +1,7 @@
 import json
 from collections import Counter
 
+
 def proverka_json_file():
     """
     отдельный запуск файла, просто для информации
@@ -22,16 +23,18 @@ def proverka_json_file():
 
     # all_link - собирает все ссылки в json
     all_link = get_all_links(data, tarifs)
+    print(f"► Всего ссылок: {len(all_link)}")
+
+    # кол-во тестов для каждого тарифа
+    count_tests_for_tarif(data, tarifs)
 
     # здесь ищутся дубли в ссылках all_link
     dubli_links = [k for k, v in Counter(all_link).items() if v > 1]
-    if len(dubli_links)!=0:
-        print(f"@@@@ Есть дубли ссылок \n{dubli_links}")
+    if len(dubli_links) != 0:
+        print(f"_____________________________________ Есть дубли ссылок \n{dubli_links}")
 
     # поиск ломанных ссылок
     find_broken_link(data)
-
-
 
 
 def find_broken_link(data):
@@ -43,7 +46,7 @@ def find_broken_link(data):
     for knowledge_link in data:
         new_links_page.append(knowledge_link.get("link_page"))
         silka = knowledge_link.get("link_page")
-        if len(silka)==0:
+        if len(silka) == 0:
             print("Отсутствкет ссылка")
             print(knowledge_link.get("name"))
             continue
@@ -52,7 +55,7 @@ def find_broken_link(data):
             count_broken_link += 1
         if ("." or "alfa" or "//") in knowledge_link.get("link_page"):
             broken_link.append(knowledge_link.get("link_page"))
-            count_broken_link+=1
+            count_broken_link += 1
         for key in knowledge_link:
             if "link_bz" in knowledge_link.get(key):
                 link = (knowledge_link.get(key)).get("link_bz")
@@ -61,12 +64,13 @@ def find_broken_link(data):
                     broken_link.append(link)
                     count_broken_link += 1
 
-    if count_broken_link !=0:
+    if count_broken_link != 0:
         print(f"► Есть ссылки для переделки в кол-ве: {count_broken_link}")
         for i in broken_link:
             print(i)
 
-def get_all_links(data,tarifs):
+
+def get_all_links(data, tarifs):
     all_link = []
     for i in data:
         all_link.append(i.get("link_page"))
@@ -74,5 +78,24 @@ def get_all_links(data,tarifs):
             if tarif in i:
                 all_link.append((i.get(tarif)).get("link_bz"))
     return all_link
+
+
+def count_tests_for_tarif(data, tarifs_all):
+    tarifs_chet = {"КИИ.ГИС.ПДН.БЮДЖЕТ.ЭКСПЕРТ": 0,
+                   "ПДН.БЮДЖЕТ.ЭКСПЕРТ": 0,
+                   "КИИ.СТАНДАРТ": 0,
+                   "КИИ.ПДН.ЭКСПЕРТ": 0,
+                   "ГИС.ЭКСПЕРТ": 0,
+                   "ГИС.ПДН.БЮДЖЕТ.ЭКСПЕРТ": 0,
+                   "ПДН.НОТАРИУС": 0,
+                   "КИИ.ГИС": 0}
+    for dat in data:
+        for tarif in tarifs_all:
+            if tarif in dat:
+                tarifs_chet.update({tarif: tarifs_chet.get(tarif) + 1})
+    print("► Кол-во тестов для каждого тарифа:")
+    for i in tarifs_chet.items():
+        print('  ', i)
+
 
 proverka_json_file()
