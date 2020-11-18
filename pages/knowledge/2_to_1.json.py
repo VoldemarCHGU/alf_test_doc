1be81b1502745cf7a8ebfe160f6294e6cc0a5de1
admin <vovnik98@mail.ru>
2020-11-18 03:29:15 +0300

    обновление

--- pages/knowledge/2_to_1.json.py
+++ pages/knowledge/2_to_1.json.py
@@ -1,7 +1,7 @@
 import json
-from collections import Counter
 
-def concat(file1,file2):
+
+def concat(file1, file2):
     """
     отдельный запуск файла, просто для информации
     """
@@ -14,18 +14,18 @@ def concat(file1,file2):
     with open(file1, encoding='utf-8') as f1:
         pars = json.load(f1)
         datas = pars.get("knowledge")
-        all_links_datas = get_all_links(datas) #все шаги в старом файле
+        all_links_datas = get_all_links(datas)  # все шаги в старом файле
 
         exists_links = []
         for new_data in new_datas:
             if new_data.get("link_page") not in all_links_datas:
                 new_dict = {}
-                new_dict['name']=new_data.get("name")
+                new_dict['name'] = new_data.get("name")
                 new_dict['link_page'] = new_data.get("link_page")
                 new_dict['type_page'] = new_data.get("type_page")
                 new_dict[new_tarif] = {
-                    'link_bz': new_data.get('link_bz') ,
-                    'zagolovok_in_knowledge':new_data.get('zagolovok_in_knowledge')
+                    'link_bz': new_data.get('link_bz'),
+                    'zagolovok_in_knowledge': new_data.get('zagolovok_in_knowledge')
                 }
                 datas.append(new_dict)
             elif new_data.get("link_page") in all_links_datas:
@@ -33,32 +33,19 @@ def concat(file1,file2):
                 for i in range(len(datas)):
                     if datas[i].get('link_page') == new_data.get('link_page'):
                         if new_tarif not in datas[i]:
-                            datas[i][new_tarif]={
-                                'link_bz': new_data.get('link_bz') ,
-                                'zagolovok_in_knowledge':new_data.get('zagolovok_in_knowledge')
-                                }
-
+                            datas[i][new_tarif] = {
+                                'link_bz': new_data.get('link_bz'),
+                                'zagolovok_in_knowledge': new_data.get('zagolovok_in_knowledge')
+                            }
 
                         break
-
-
-
 
     file_path = file1
     with open(file_path, 'w', encoding='utf-8') as outfile:
         json.dump(datas, outfile, ensure_ascii=False)
     print()
 
-
-
-
-
-
-
-
-
     print()
-
 
 
 def get_all_links(data):
@@ -70,6 +57,4 @@ def get_all_links(data):
     return all_link
 
 
-concat("PDN_new.json", "КИИ_ГИС_ПДн_Бюджет_Эксперт.json")
-
-
+concat("knowledge_data.json", "КИИ_ГИС_ПДн_Бюджет_Эксперт.json")
