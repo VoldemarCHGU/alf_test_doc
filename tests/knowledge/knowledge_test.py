e21913fb3b4e626b08d59bfd3b041a5ef03c77a0
admin <vovnik98@mail.ru>
2020-11-18 03:25:23 +0300

    Добавлена проверка_на_skip_test

--- tests/knowledge/knowledge_test.py
+++ tests/knowledge/knowledge_test.py
@@ -1,12 +1,13 @@
 import pytest
 
-from .pages.knowledge_locators import KnowledgeLocators
-from .pages.knowledge_page import KnowledgePageBeforeMoving
+from pages.knowledge.knowledge_functions import проверка_на_skip_test
+from pages.knowledge.knowledge_locators import KnowledgeLocators
+from pages.knowledge.knowledge_page import KnowledgePageBeforeMoving
 
-
+@pytest.mark.AFD_5840
 @pytest.mark.knowledge1
+@pytest.mark.parametrize('data', KnowledgeLocators.DATA_TARIFF_ПДН_БЮДЖЕТ_ЭКСПЕРТ, )
 class TestKnowledge1():
-    @pytest.mark.parametrize('data', KnowledgeLocators.DATA_TARIFF_ПДН_БЮДЖЕТ_ЭКСПЕРТ, )
     def test_проверка_перехода_в_базу_знаний_ПДН_БЮДЖЕТ_ЭКСПЕРТ(self, browser1, data):
         тариф_для_проверки = (data, "ПДН.БЮДЖЕТ.ЭКСПЕРТ")
         page = KnowledgePageBeforeMoving(browser1, data)
@@ -16,11 +17,11 @@ class TestKnowledge1():
         page.работоспособность_ссылки(*тариф_для_проверки)
         page.переход_в_базу_знаний(*тариф_для_проверки)
 
-
+@pytest.mark.AFD_5840
 @pytest.mark.knowledge2
 class TestKnowledge2():
     @pytest.mark.parametrize('data', KnowledgeLocators.DATA_TARIFF_КИИ_СТАНДАРТ)
-    def test_проверка_перехода_в_базу_КИИ_СТАНДАРТ(self, browser2, data):
+    def test_проверка_перехода_в_базу_знаний_КИИ_СТАНДАРТ(self, browser2, data):
         тариф_для_проверки = (data, "КИИ.СТАНДАРТ")
         page = KnowledgePageBeforeMoving(browser2, data)
         page.есть_синяя_плашка()
@@ -29,15 +30,17 @@ class TestKnowledge2():
         page.работоспособность_ссылки(*тариф_для_проверки)
         page.переход_в_базу_знаний(*тариф_для_проверки)
 
-
+@pytest.mark.AFD_5840
 @pytest.mark.knowledge3
 class TestKnowledge3():
     @pytest.mark.parametrize('data', KnowledgeLocators.DATA_TARIFF_КИИ_ГИС_ПДН_БЮДЖЕТ_ЭКСПЕРТ)
-    def test_проверка_перехода_в_базу_КИИ_ГИС_ПДН_БЮДЖЕТ_ЭКСПЕРТ(self, browser3, data):
+    def test_проверка_перехода_в_базу_знаний_КИИ_ГИС_ПДН_БЮДЖЕТ_ЭКСПЕРТ(self, browser_КИИ_ГИС_ПДН_БЮДЖЕТ_ЭКСПЕРТ, data):
         тариф_для_проверки = (data, "КИИ.ГИС.ПДН.БЮДЖЕТ.ЭКСПЕРТ")
-        page = KnowledgePageBeforeMoving(browser3, data)
-        page.есть_синяя_плашка()
+        проверка_на_skip_test(*тариф_для_проверки)
+        page = KnowledgePageBeforeMoving(browser_КИИ_ГИС_ПДН_БЮДЖЕТ_ЭКСПЕРТ, data)
+        page.есть_синяя_плашка(data)
         page.есть_текст_в_плашке()
         page.ссылка_в_синей_плашке()
         page.работоспособность_ссылки(*тариф_для_проверки)
-        page.переход_в_базу_знаний(*тариф_для_проверки)
+        # page.переход_в_базу_знаний(*тариф_для_проверки)
+
