74c1f5e1f8aae981e4ac1e1d3c6573fba58ec96e
admin <vovnik98@mail.ru>
2020-12-24 03:57:04 +0300

    скрин при открытии страницы + проверка текущего URL после перехода

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,12 @@
 import time
 
+import allure
+from allure_commons.types import AttachmentType
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+from .main_functions import проверка_ссылки
 from .main_settings import MAIN_URL
 
 
@@ -11,13 +14,24 @@ class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url.get("link_page").strip()
+
+        проверка_ссылки(MAIN_URL + self.url)
         self.open_url()
+
+        self.ожидание_прогрузки_страницы()
+        with allure.step("Открытие страницы"):
+            allure.attach(MAIN_URL + self.url,
+                          name="Ссылка")
+            allure.attach(self.browser.get_screenshot_as_png(),
+                          name='screenshot',
+                          attachment_type=AttachmentType.PNG)
 
     def open_url(self):
         """
         открывает ссылку внутри (авторизированному пользователю)
         :return:
         """
+
         url = MAIN_URL + self.url
         self.browser.get(url)
         self.ожидание_прогрузки_страницы()
@@ -35,6 +49,14 @@ class BasePage():
         try:
             while loadind:
                 self.browser.find_element(By.CSS_SELECTOR, ".loading")
-                time.sleep(1)
+                time.sleep(0.5)
         except:
             pass
+
+    def проверка_url_в_адресной_строке(self, url):
+        self.ожидание_прогрузки_страницы()
+        current_url = self.browser.current_url
+        assert url == current_url, \
+            f"""Не та страница
+            ♦Ожидалось: {url}
+            ♦Открылось: {current_url}"""
